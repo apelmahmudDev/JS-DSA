0f189fac401d66e24eafc99e14696e606d6970c4
Apel Mahmud <apelmahmud.dev@gmail.com>
2024-08-18 21:30:39 +0600

    dsa: bubble sort implementation

--- bubble-sort/bubble-sort.js
+++ bubble-sort/bubble-sort.js
@@ -1,2 +1,21 @@
-const age = [34, 33, 21, 55, 80, 10, 44, 15];
+let age = [34, 33, 21, 55, 80, 10, 44, 15];
 
+const bubbleSort = (arr) => {
+  let swapped = false;
+  let n = arr.length;
+
+  do {
+    swapped = false;
+    for (let i = 1; i < n; i++) {
+      if (arr[i - 1] > arr[i]) {
+        [arr[i - 1], arr[i]] = [arr[i], arr[i - 1]];
+        swapped = true;
+      }
+    }
+    n--;
+  } while (swapped);
+
+  return arr;
+};
+
+console.log(bubbleSort(age));
